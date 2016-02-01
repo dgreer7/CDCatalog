c75caf4e017bda59f7f4647732026493e22c2eff
David <dgreer7@gmail.com>
2016-02-01 03:12:52 -0800

    Initial. Contains entity and tabs with tab/window resize logic.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CDCatalog
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeepTabSizedWithWindow();
+
+            using (CDCatalogEntities CDCatalogEntity = new CDCatalogEntities())
+            {
+
+            }
+        }
+
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            KeepTabSizedWithWindow();
+        }
+
+        private void KeepTabSizedWithWindow()
+        {
+            tabControlMain.Width = this.ClientSize.Width;
+            tabControlMain.Height = this.ClientSize.Height;
+        }
+    }
+}
