a4201dd88c7014cfea628099ce94389cc03d504c
David <dgreer7@gmail.com>
2016-02-01 03:36:02 -0800

    Updated tab names and text

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.tabControlMain = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabAddSong = new System.Windows.Forms.TabPage();
+            this.tabAddAlbum = new System.Windows.Forms.TabPage();
             this.tabControlMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControlMain
             // 
-            this.tabControlMain.Controls.Add(this.tabPage1);
-            this.tabControlMain.Controls.Add(this.tabPage2);
+            this.tabControlMain.Controls.Add(this.tabAddSong);
+            this.tabControlMain.Controls.Add(this.tabAddAlbum);
             this.tabControlMain.Location = new System.Drawing.Point(1, 1);
             this.tabControlMain.Name = "tabControlMain";
             this.tabControlMain.SelectedIndex = 0;
@@ -45,25 +45,25 @@
             this.tabControlMain.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
             this.tabControlMain.TabIndex = 0;
             // 
-            // tabPage1
+            // tabAddSong
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(522, 376);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabAddSong.Location = new System.Drawing.Point(4, 25);
+            this.tabAddSong.Name = "tabAddSong";
+            this.tabAddSong.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAddSong.Size = new System.Drawing.Size(522, 376);
+            this.tabAddSong.TabIndex = 0;
+            this.tabAddSong.Text = "Add Song";
+            this.tabAddSong.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // tabAddAlbum
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(520, 420);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabAddAlbum.Location = new System.Drawing.Point(4, 25);
+            this.tabAddAlbum.Name = "tabAddAlbum";
+            this.tabAddAlbum.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAddAlbum.Size = new System.Drawing.Size(522, 376);
+            this.tabAddAlbum.TabIndex = 1;
+            this.tabAddAlbum.Text = "Add Album";
+            this.tabAddAlbum.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
@@ -83,8 +83,8 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControlMain;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabAddSong;
+        private System.Windows.Forms.TabPage tabAddAlbum;
     }
 }
 
