7f7d69c68e5a2d3537e5a643bc05a03a53ca56c8
David <dgreer7@gmail.com>
2016-03-16 19:56:47 -0700

    Removed old form and connector. Wrapped everything in try catch.

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -5,6 +5,8 @@
 
     public partial class MainForm : Form
     {
+        private const string ERRORMESSAGE = "Way to go, you broke it.\n";
+
         public MainForm()
         {
             InitializeComponent();
@@ -18,191 +20,266 @@
         private void KeepDataGridSizedWithWindow()
         {
             mainFormDataGridView.Width = ClientSize.Width;
-            mainFormDataGridView.Height = ClientSize.Height-menuStrip1.Height;
+            mainFormDataGridView.Height = ClientSize.Height - menuStrip1.Height;
         }
 
         private void MainForm_Resize(object sender, EventArgs e)
         {
-            //Upon resizing of form, keep datagrid sized with the window
-            KeepDataGridSizedWithWindow();
+            try
+            {
+                //Upon resizing of form, keep datagrid sized with the window
+                KeepDataGridSizedWithWindow();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
+            }
+
         }
 
         private void genreToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            var addGenre = new AddGenre();
-            addGenre.Show();
+            try
+            {
+                var addGenre = new AddGenre();
+                addGenre.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
+            }
         }
 
         private void albumnToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            var addAlbum = new AddAlbum();
-            addAlbum.Show();
+            try
+            {
+                var addAlbum = new AddAlbum();
+                addAlbum.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
+            }
+
         }
 
         private void artistToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var addArtist = new AddArtist();
-            addArtist.Show();
+            try
+            {
+                var addArtist = new AddArtist();
+                addArtist.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
+            }
+
         }
 
         private void songToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            var addSong = new AddSong();
-            addSong.Show();
+            try
+            {
+                var addSong = new AddSong();
+                addSong.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
+            }
+
         }
 
         private void findGenreToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var findGenre = new FindGenre();
-            var dialogResult = findGenre.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            try
             {
-                mainFormDataGridView.DataSource = findGenre.FoundGenres;
-                for (int i = 0; i < mainFormDataGridView.Columns.Count; i++)
+                var findGenre = new FindGenre();
+                var dialogResult = findGenre.ShowDialog();
+
+                if (dialogResult == DialogResult.OK)
                 {
-                    string columnHeaderName = mainFormDataGridView.Columns[i].HeaderText;
-                    if (columnHeaderName == "GenreName")
+                    mainFormDataGridView.DataSource = findGenre.FoundGenres;
+                    for (int i = 0; i < mainFormDataGridView.Columns.Count; i++)
                     {
-                        mainFormDataGridView.Columns[i].HeaderText = "Genre Name";
-                        mainFormDataGridView.Columns[i].Width = mainFormDataGridView.Width;
-                    }
-                    else
-                    {
-                        mainFormDataGridView.Columns.Remove(columnHeaderName);
-                        i--;
+                        string columnHeaderName = mainFormDataGridView.Columns[i].HeaderText;
+                        if (columnHeaderName == "GenreName")
+                        {
+                            mainFormDataGridView.Columns[i].HeaderText = "Genre Name";
+                            mainFormDataGridView.Columns[i].Width = mainFormDataGridView.Width;
+                        }
+                        else
+                        {
+                            mainFormDataGridView.Columns.Remove(columnHeaderName);
+                            i--;
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
             }
         }
 
         private void findAlbumToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var findAlbum = new FindAlbum();
-            var dialogResult = findAlbum.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            try
             {
-                mainFormDataGridView.DataSource = findAlbum.FoundAlbums;
-                for (int i = 0; i < mainFormDataGridView.Columns.Count; i++)
+                var findAlbum = new FindAlbum();
+                var dialogResult = findAlbum.ShowDialog();
+
+                if (dialogResult == DialogResult.OK)
                 {
-                    string columnHeaderName = mainFormDataGridView.Columns[i].HeaderText;
-                    if (columnHeaderName == "Title" || columnHeaderName == "Year" || columnHeaderName == "Rating" || columnHeaderName == "Artist_Name")
+                    mainFormDataGridView.DataSource = findAlbum.FoundAlbums;
+                    for (int i = 0; i < mainFormDataGridView.Columns.Count; i++)
                     {
-                        if (columnHeaderName == "Artist_Name")
+                        string columnHeaderName = mainFormDataGridView.Columns[i].HeaderText;
+                        if (columnHeaderName == "Title" || columnHeaderName == "Year" || columnHeaderName == "Rating" || columnHeaderName == "Artist_Name")
                         {
-                            mainFormDataGridView.Columns[i].HeaderText = "Artist";
-                        }
+                            if (columnHeaderName == "Artist_Name")
+                            {
+                                mainFormDataGridView.Columns[i].HeaderText = "Artist";
+                            }
                             mainFormDataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+                        }
+                        else
+                        {
+                            mainFormDataGridView.Columns.Remove(columnHeaderName);
+                            i--;
+                        }
+                        mainFormDataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                     }
-                    else
-                    {
-                        mainFormDataGridView.Columns.Remove(columnHeaderName);
-                        i--;
-                    }
-                    mainFormDataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
             }
         }
 
         private void findSongToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var findSong = new FindSong();
-            var dialogResult = findSong.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            try
             {
-                mainFormDataGridView.DataSource = findSong.FoundSongs;
-                var titleHeaderIndex = 0;
+                var findSong = new FindSong();
+                var dialogResult = findSong.ShowDialog();
 
-                for (int i = 0; i < mainFormDataGridView.Columns.Count; i++)
+                if (dialogResult == DialogResult.OK)
                 {
-                    
-                    if (mainFormDataGridView.Columns[i].HeaderText == "Track_Length_Seconds")
+                    mainFormDataGridView.DataSource = findSong.FoundSongs;
+                    var titleHeaderIndex = 0;
+
+                    for (int i = 0; i < mainFormDataGridView.Columns.Count; i++)
                     {
-                        mainFormDataGridView.Columns[i].HeaderText = "Length (Sec)";
+
+                        if (mainFormDataGridView.Columns[i].HeaderText == "Track_Length_Seconds")
+                        {
+                            mainFormDataGridView.Columns[i].HeaderText = "Length (Sec)";
+                        }
+
+                        string columnHeaderName = mainFormDataGridView.Columns[i].HeaderText;
+
+                        if (columnHeaderName == "SongID")
+                        {
+                            mainFormDataGridView.Columns.Remove(columnHeaderName);
+                            i--;
+                        }
+
+                        else if (columnHeaderName == "Title")
+                            titleHeaderIndex = i;
+
+                        else
+                            mainFormDataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+
+                        mainFormDataGridView.Columns[titleHeaderIndex].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                     }
-
-                    string columnHeaderName = mainFormDataGridView.Columns[i].HeaderText;
-
-                    if (columnHeaderName == "SongID")
-                    {
-                        mainFormDataGridView.Columns.Remove(columnHeaderName);
-                        i--;
-                    }
-
-                    else if (columnHeaderName == "Title")
-                        titleHeaderIndex = i;
-
-                    else
-                        mainFormDataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
-
-                    mainFormDataGridView.Columns[titleHeaderIndex].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
             }
         }
 
         private void playlistToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var createPlaylist = new CreatePlaylist();
-            var dialogResult = createPlaylist.ShowDialog();
-
-            if (dialogResult == DialogResult.OK)
+            try
             {
-                mainFormDataGridView.DataSource = createPlaylist.Playlist;
+                var createPlaylist = new CreatePlaylist();
+                var dialogResult = createPlaylist.ShowDialog();
+
+                if (dialogResult == DialogResult.OK)
+                {
+                    mainFormDataGridView.DataSource = createPlaylist.Playlist;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
             }
         }
 
         private void ratingToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (mainFormDataGridView.RowCount == 0)
+            try
             {
-                MessageBox.Show("Please find either a song or album to update.");
-            }
-            else if (mainFormDataGridView.SelectedRows.Count != 1)
-            {
-                MessageBox.Show("Please select the song or album to update.");
-            }
-            else
-            {
-                //will get the type of data stored in the datagridview
-                var selectedValue = mainFormDataGridView.SelectedRows[0].DataBoundItem;
-                if (selectedValue.GetType() == typeof(SongView))
+                if (mainFormDataGridView.RowCount == 0)
                 {
-                    //cast selected line as a SongView
-                    var selectedSong = selectedValue as SongView;
-
-                    var updateSong = new UpdateSong(selectedSong.Title, selectedSong.Album);
-                    var dialogResult = updateSong.ShowDialog();
-
-                    if (dialogResult == DialogResult.OK)
-                    {
-                        //Clear the datagridview
-                        mainFormDataGridView.DataSource = null;
-                        mainFormDataGridView.Rows.Clear();
-                        mainFormDataGridView.Refresh();
-                    }
+                    MessageBox.Show("Please find either a song or album to update.");
                 }
-                else if (selectedValue.GetType() == typeof(AlbumView))
+                else if (mainFormDataGridView.SelectedRows.Count != 1)
                 {
-                    //cast selected line as a AlbumView
-                    var selectedAlbum = selectedValue as AlbumView;
-
-                    var albumView = new UpdateAlbum(selectedAlbum.Title, selectedAlbum.Artist_Name);
-                    var dialogResult = albumView.ShowDialog();
-
-                    if (dialogResult == DialogResult.OK)
-                    {
-                        //Clear the datagridview
-                        mainFormDataGridView.DataSource = null;
-                        mainFormDataGridView.Rows.Clear();
-                        mainFormDataGridView.Refresh();
-                    }
+                    MessageBox.Show("Please select the song or album to update.");
                 }
                 else
                 {
-                    MessageBox.Show(string.Format("Please find either a song or album to update", "Data validation error"));
+                    //will get the type of data stored in the datagridview
+                    var selectedValue = mainFormDataGridView.SelectedRows[0].DataBoundItem;
+                    if (selectedValue.GetType() == typeof(SongView))
+                    {
+                        //cast selected line as a SongView
+                        var selectedSong = selectedValue as SongView;
+
+                        var updateSong = new UpdateSong(selectedSong.Title, selectedSong.Album);
+                        var dialogResult = updateSong.ShowDialog();
+
+                        if (dialogResult == DialogResult.OK)
+                        {
+                            //Clear the datagridview
+                            mainFormDataGridView.DataSource = null;
+                            mainFormDataGridView.Rows.Clear();
+                            mainFormDataGridView.Refresh();
+                        }
+                    }
+                    else if (selectedValue.GetType() == typeof(AlbumView))
+                    {
+                        //cast selected line as a AlbumView
+                        var selectedAlbum = selectedValue as AlbumView;
+
+                        var albumView = new UpdateAlbum(selectedAlbum.Title, selectedAlbum.Artist_Name);
+                        var dialogResult = albumView.ShowDialog();
+
+                        if (dialogResult == DialogResult.OK)
+                        {
+                            //Clear the datagridview
+                            mainFormDataGridView.DataSource = null;
+                            mainFormDataGridView.Rows.Clear();
+                            mainFormDataGridView.Refresh();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(string.Format("Please find either a song or album to update", "Data validation error"));
+                    }
                 }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ERRORMESSAGE + ex.GetBaseException().Message, "Nice job!");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -12,9 +12,16 @@
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Holy cow, I can't believe you managed to screw this up so bad.\n" + ex.GetBaseException().Message, "Thanks a lot!");
+            }
         }
     }
 }
